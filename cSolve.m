5b3f5347751026ac65949cd69861fde0acbfa47c
Mohammad Mirzadeh <mirzadeh@gmail.com>
2018-06-13 12:26:16 -0400

    fixed gradient and NP solver

--- cSolve.m
+++ cSolve.m
@@ -1,14 +1,10 @@
-function [cn] = cSolve(x,dx,dt,psi,c,z,d)
-Nx = length(x);
-[y,dy] = MAC(x);
+function cn = cSolve(dx, dt, psi, c, z, d, bc)
+f = getFlux(dx, c, psi, z, d, 'linear');
+F = c/dt - diff(f) ./ dx;
 
-f = getFlux(x,dx,c,psi,z,d); F=y;
-
-F(1) = c(1)/dt - (f(2)-f(1))*2/(dy(1)+dy(1));
-for i=2:Nx-2
-    F(i) = c(i)/dt - (f(i+1)-f(i))*2/(dy(i)+dy(i-1));
-end
-F(Nx-1) = c(Nx-1)/dt - (f(Nx)-f(Nx-1))*2/(dy(Nx-2)+dy(Nx-2));
+% apply flux boundary conditions
+F(1) = F(1) + bc(1)/dx(1);
+F(end) = F(end) - bc(end)/dx(end);
 
 A = matGen(y,dy,0,0,dt,'NP', d);
 cn = A\F;
--- grad.m
+++ grad.m
@@ -1,11 +1,17 @@
-function [du] = grad(x,dx,u)
-Nx = length(x);
-
-du = zeros(Nx-1,1);
-
-for i=1:Nx-1
-%    du(i) = 0.5*((u(i+1)-u(i))/dx(i)+(u(i)-u(i-1))/dx(i-1));
-    du(i) = (u(i+1)-u(i))/dx(i);
+function du = grad(u, x, dx, varargin)
+if nargin < 3
+    type = 'node';
+else
+    type = lower(varargin{1});
 end
 
+switch type
+    case 'node'
+        du = cell2node(diff(u) ./ dx);
+    case 'cell'
+        xc = x(1:end-1) + 0.5*dx;
+        du = diff(u) ./ diff(xc);
+        dul = inter(x(2), x(3), du(1), du(2), x(1));
+        dur = inter(x(end-1), x(end-2), du(end), du(end-1), x(end));
+        du = [dul, du, dur];
 end
