f15ba375d1cc19db4266e09219da2a654f000768
Mohammad Mirzadeh <mirzadeh@gmail.com>
2018-06-14 01:17:15 -0400

    Added readme

--- PBSolver.m
+++ PBSolver.m
@@ -1,4 +1,4 @@
-function sol = PBSolver(x, lambda, bc)
+function sol = PBSolver(x, lambda, v)
 sol.grid = struct('x', x);
 sol.options = struct('itmax', 100, 'tol', 1e-6, 'omega', 1);
 sol.psi = zeros(size(x));
@@ -8,7 +8,7 @@ err = inf;
 A = matGen(x);
 while err > sol.options.tol
     rhs = 1/lambda^2*(sol.psi.*cosh(sol.psi) - sinh(sol.psi));
-    rhs(1) = bc(1); rhs(end) = bc(end);
+    rhs(1) = -v; rhs(end) = v;
     
     D = 1/lambda^2 * sparse(diag(cosh(sol.psi)));
     D(1) = 0; D(end) = 0;
