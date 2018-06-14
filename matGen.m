6ac076c2ab5e873568c851cd912c201b5acd1a10
Mohammad Mirzadeh <mirzadeh@gmail.com>
2018-06-14 00:09:14 -0400

    fixed neumann matrix

--- matGen.m
+++ matGen.m
@@ -1,29 +1,37 @@
 function A = matGen(x, varargin)
 dx = diff(x);
-nx = length(x);
 
 if nargin < 2
-    bc = ["neumann", "neumann"];
+    type = 'node';
 else
-    bc = string(lower(varargin{1}));
+    type = 'cell';
 end
 
-dxl = [dx(1); dx];
-dxr = [dx; dx(end)];
-wl = -2./(dxl.*(dxl+dxr)); wl(1) = 0;
-wr = -2./(dxr.*(dxl+dxr)); wr(end) = 0;
-wc = -(wr+wl);
-
-if strcmp(bc(1), 'dirichlet')
-    wc(1) = 1;
-    wr(1) = 0;
-end
-
-if strcmp(bc(end), 'dirichlet')
-    wc(end) = 1;
-    wl(end) = 0;
+switch type
+    case 'node'
+        nx = length(x);
+        dxl = [dx(1); dx];
+        dxr = [dx; dx(end)];
+        
+        wl = -2./(dxl.*(dxl+dxr)); wl(1) = 0;
+        wr = -2./(dxr.*(dxl+dxr)); wr(end) = 0;
+        wc = -(wr+wl);
+        
+        % apply bc
+        wc(1) = 1; wc(end) = 1;
+        wr(1) = 0; wl(end) = 0;
+        
+    case 'cell'
+        xc = x(1:end-1) + 0.5*dx;
+        nx = length(xc);
+        dxc = diff(xc);
+        dxl = [dxc(1); dxc];
+        dxr = [dxc; dxc(end)];
+        
+        wl = -1./(dxl.*dx); wl(1) = 0;
+        wr = -1./(dxr.*dx); wr(end) = 0;
+        wc = -(wr+wl);     
 end
 
 A = spdiags([wr wc wl], [-1 0 1], nx, nx)';
-
 end
